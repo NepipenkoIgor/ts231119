5b8870506af0e25dcf15faa9029d6a42ef7329fe
Ihor <igornepipenko@gmail.com>
2019-11-23 15:26:40 +0200

    Feature: base types [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,44 @@
-import { speakSloth } from './features';
-import { speakSloth1 } from './features-1';
-import { speakSloth2 } from './features-2';
-import { speakSloth3 } from './features-3';
-import { speakSloth4 } from './features-4';
+interface UserAccount {
+    firstName: string;
+    age: number;
+}
 
-speakSloth(['ts', 'is', 'awesome']);
-speakSloth1(['ts1', 'is1', 'awesome1']);
-speakSloth2(['ts2', 'is2', 'awesome2']);
-speakSloth3(['ts3', 'is3', 'awesome3']);
-speakSloth4(['ts4', 'is4', 'awesome4']);
+let p1 = UserAccount;
 
+
+const userAccount = {
+    firstName: 'Ihor',
+    age: 33,
+};
+
+let p2: typeof userAccount = {
+    firstName: 'Ihor',
+    age: 33,
+};
+
+
+const s;
+let a: typeof s = 'asd';
+
+
+let isDone: boolean = false;
+let str: string = 'some value';
+let num: number = null;
+let bigNum: bigint = 4n;
+
+let und: undefined = undefined;
+let nill: null = null;
+
+const key1: symbol = Symbol('key1');
+const key2: unique symbol = Symbol('key2');
+const key3 = Symbol('key3');
+
+let strictObject = {
+    [key1]: 100,
+    [key2]: 'Some value',
+    [key3]: true,
+};
+let a1 = strictObject[key1];
+let a2 = strictObject[key2];
+let a3 = strictObject[key3];
 
