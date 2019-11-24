e4785f962582a3047bc542b5132229f2d1b2635f
Ihor <igornepipenko@gmail.com>
2019-11-24 12:25:49 +0200

    Feature: generic base [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,36 @@
-// interface IPoint {
-//     x: number;
-//     y: number;
-// }
-//
-// type Point = {
-//     x: number;
-//     y: number;
-// }
-//
-// let obj1: {
-//     x: number;
-//     y: number;
-// };
-//
-// let obj2: IPoint;
-//
-// let obj3: Point;
+// interfaces,  alias , functions, classes
 
-// interface IFn {
-//     (x: number, y: number): number;
-// }
-//
-// type Fn = (x: number, y: number) => number;
-//
-// let fn: (x: number, y: number) => number = (_x: number, _y: number) => {
-//     return 2;
-// };
-// let fn1: IFn = (_x: number, _y: number) => {
-//     return 2;
-// };
-// let fn2: Fn = (_x: number, _y: number) => {
-//     return 2;
-// };
-//
-// let fn3: (x: number, y: number) => number;
+interface IAccount<GeneralInfo extends { male: boolean }, ID = string> {
+    id: ID;
+    name: string;
+    info: GeneralInfo;
+}
 
-// type PointX = { x: number };
-//
-// interface IPointY {
-//     y: number;
-// }
-//
-// type Point = PointX & IPointY;
-//
-// interface IPoint extends PointX, IPointY {
-//
-// }
-//
-// let p1: Point = {
-//     x: 1,
-//     y: 1,
-// };
-//
-// let p2: IPoint = {
-//     x: 1,
-//     y: 1,
-// };
-//
-// class Point1 implements Point {
-//     x = 1;
-//     y = 1;
-// }
-// class Point2 implements IPoint {
-//     x = 1;
-//     y = 1;
-// }
-//  type SNB = string | number | boolean;
-
-
-// type Point =  {
-//     y: number;
-// };
-// type Point =  {
-//     x: number;
-// };
-
-
-let p: Point = {
-    x: 1,
-    y: 1,
+let admin: IAccount<{ email: string, male: boolean }> = {
+    id: 'asdaq2123Dawq1e',
+    name: 'Ihor',
+    info: {
+        male: true,
+        email: 'info@gmail',
+    },
 };
 
+let user: IAccount<{ phone: number, male: boolean }, number> = {
+    id: 111,
+    name: 'Ihor',
+    info: {
+        male: true,
+        phone: 123113231,
+    },
+};
 
-interface Point {
-    y: number;
+let str: string = 'sa';
+
+
+function getProperty<Obj, Key extends keyof Obj>(obj: Obj, k: Key) {
+    return obj[k];
 }
 
-interface Point {
-    x: number;
-}
+const key = 'subjects';
 
-let a: Record
+getProperty(user, key);
