0d08333d55664dc130f4ad47be61d42f4c637e2f
Ihor <igornepipenko@gmail.com>
2019-11-24 18:37:04 +0200

    Feature: run time check [ref:no-ref]

--- src/index.js
+++ src/index.js
@@ -1,43 +1,38 @@
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length,
-        r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-
-class Search {
-    onSearch(e) {
-        const value = e.target.value;
-        console.log(value);
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+function checkTypeInRunTime(target, key) {
+    const { name: typeName } = Reflect.getMetadata('design:type', target, key);
+    let val;
+    Object.defineProperty(target, key, {
+        get() {
+            return val;
+        },
+        set(v) {
+            if (typeof v !== typeName.toLowerCase()) {
+                throw new Error(`type for ${key} is not ${typeName}`);
+            }
+            val = v;
+        },
+    });
+}
+class Person {
+    setFullName(_middle, _surname) {
+        return 1;
     }
 }
-
 __decorate([
-    debounce(500)
-], Search.prototype, "onSearch", null);
-const search = new Search();
-const input = document.querySelector('input');
-input.addEventListener('input', search.onSearch);
-
-function debounce(ms) {
-    let timeoutId;
-    return function (_target, _key, descriptor) {
-        console.log(_target);
-        console.log(_key);
-        console.log(descriptor);
-        const originalValue = descriptor.value;
-        return {
-            ...descriptor,
-            value: (...args) => {
-                if (timeoutId) {
-                    clearTimeout(timeoutId);
-                }
-                timeoutId = setTimeout(() => {
-                    originalValue(...args);
-                }, ms);
-            },
-        };
-    };
-}
+    checkTypeInRunTime,
+    __metadata("design:type", String)
+], Person.prototype, "firstName", void 0);
+const p = new Person();
+p.firstName = [];
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
-class Search {
-    @debounce(500)
-    public onSearch(e: Event) {
-        const value = (e.target as HTMLInputElement).value;
-        console.log(value);
+import 'reflect-metadata';
+
+function checkTypeInRunTime(target: Object, key: string) {
+    const {name: typeName} = Reflect.getMetadata('design:type', target, key);
+    let val: unknown;
+    Object.defineProperty(target, key, {
+        get() {
+            return val;
+        },
+        set(v: any): void {
+            if (typeof v !== typeName.toLowerCase()) {
+                throw new Error(`type for ${key} is not ${typeName}`);
+            }
+            val = v;
+        },
+    });
+}
+
+// function logMethodMetaData(target: Object, key: string, _descriptor: PropertyDescriptor) {
+//     console.log(Reflect.getMetadata('design:type', target, key));
+//     console.log(Reflect.getMetadata('design:paramtypes', target, key));
+//     console.log(Reflect.getMetadata('design:returntype', target, key));
+//     console.log(Reflect.getMetadata('design:typeinfo', target, key));
+// }
+
+class Person {
+    @checkTypeInRunTime
+    public firstName!: string;
+
+    public setFullName(_middle: string, _surname: string): number {
+        return 1;
     }
+
 }
 
-const search = new Search();
+const p = new Person();
 
-const input = document.querySelector('input') as HTMLInputElement;
-input.addEventListener('input', search.onSearch);
-
-function debounce(ms: number) {
-    let timeoutId: number | null;
-    return function (_target: Object, _key: string | symbol, descriptor: PropertyDescriptor) {
-        console.log(_target);
-        console.log(_key);
-        console.log(descriptor);
-        const originalValue = descriptor.value;
-        return {
-            ...descriptor,
-            value: (...args: unknown[]) => {
-                if (timeoutId) {
-                    clearTimeout(timeoutId);
-                }
-                timeoutId = setTimeout(() => {
-                    originalValue(...args);
-                }, ms);
-            },
-        };
-    };
-}
+(p.firstName as any) = [];
