738e2c59950c8a3b24f19988f846e06c68d1f2f5
Ihor <igornepipenko@gmail.com>
2019-11-24 13:04:53 +0200

    Feature: condition types  [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,28 @@
-// interfaces,  alias , functions, classes
+// T extends U ? X : N
 
-interface IAccount<GeneralInfo extends { male: boolean }, ID = string> {
-    id: ID;
-    name: string;
-    info: GeneralInfo;
+type notUndefined<T> = T extends undefined | null ? never : T;
+const nonUnd: notUndefined<string | undefined | null> = 1;
+
+
+const arr: [(a: string) => { name: string, age: number }, string] =
+    [() => ({name: 'Ihor', age: 33}), '1asd1241'];
+
+type FirstType<T> =
+    T extends [infer U, ...unknown[]]
+        ? U extends (...args: infer Z) => any ? Z : never
+        : T;
+
+const bool: FirstType<typeof arr> = [1];
+
+type FunctionParamsAndReturn<T> = T extends (...args: infer Args) => infer R ? Args | R : never;
+
+function fn(_a: string, _cb: (s: string) => boolean): number {
+    return 1;
 }
 
-let admin: IAccount<{ email: string, male: boolean }> = {
-    id: 'asdaq2123Dawq1e',
-    name: 'Ihor',
-    info: {
-        male: true,
-        email: 'info@gmail',
-    },
-};
+const c: FunctionParamsAndReturn<typeof fn> = {a: 1};
 
-let user: IAccount<{ phone: number, male: boolean }, number> = {
-    id: 111,
-    name: 'Ihor',
-    info: {
-        male: true,
-        phone: 123113231,
-    },
-};
-
-let str: string = 'sa';
+type Acc = { name: string, age: number } | { male: true, info: { salary: number } } | null;
 
 
-function getProperty<Obj, Key extends keyof Obj>(obj: Obj, k: Key) {
-    return obj[k];
-}
-
-const key = 'subjects';
-
-getProperty(user, key);
+const nonNullAccount: NonNullable<Acc> = 1;
