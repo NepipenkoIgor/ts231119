a5205b982a0444a0f5ad208bff6d3aa53f8eabfc
Ihor <igornepipenko@gmail.com>
2019-11-24 16:27:13 +0200

    Feature: this in function [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,114 +1,36 @@
-// function average(a: number, b: number, c: number): string {
-//     const avg: number = (a + b + c) / 3;
-//     return `Average is ${avg}`;
-// }
-//
-// average(1);
-// average(1, 2, 's');
-// const num: number = average(1, 2, 1);
-
-// function average(a: number, b?: number, c?: number): string {
-//     if (b === undefined) {
-//         b = 0;
-//     }
-//     if (c === undefined) {
-//         c = 0;
-//     }
-//     const avg: number = (a + b + c) / 3;
-//     return `Average is ${avg}`;
-// }
-//
-// average(1);
-// average(1, 2);
-// average(1, 2, 's');
-// const num: number = average(1, 2, 1);
-
-
-// function average(a: number, b: number = 0, c: number = 0): string {
-//     const avg: number = (a + b + c) / 3;
-//     return `Average is ${avg}`;
-// }
-//
-// average(1);
-// average(1, 2);
-// average(1, 2, 's');
-// const num: number = average(1, 2, 1);
-// type sn = string | number;
-//
-// function isString(item: sn): item is string {
-//     return typeof item === 'string';
-// }
-//
-// function average(...args: sn[]): string {
-//     let total: number = 0;
-//     for (const item of args) {
-//         if (isString(item)) {
-//             total += Number(item);
-//             continue;
-//         }
-//         total += item;
-//     }
-//     const avg: number = total / args.length;
-//     return `Average is ${avg}`;
-// }
-//
-// Array.isArray();
-// average(1);
-// average(1, 2);
-// average(1, 2, 2, 3, 4, 5, 6, 5);
-// average(1, '2', 2, '3', 4, 5, 6, 5);
-// average(1, 2, 's');
-// const num: number = average(1, 2, 1);
-
-
-type sn = string | number;
-
-function isString(item: sn): item is string {
-    return typeof item === 'string';
+function getFullName(this: { name: string, surname: string }) {
+    return `${this.name} ${this.surname}`;
 }
 
-function average(a: number, b: string): string;
-function average(a: number, b: number, c: string): string;
-function average(a: string, b: number): string;
-function average(_a: number | string, _b: number | string, _c?: string): string {
-    // let total: number = 0;
-    // for (const item of args) {
-    //     if (isString(item)) {
-    //         total += Number(item);
-    //         continue;
-    //     }
-    //     total += item;
-    // }
-    // const avg: number = total / args.length;
-    // return `Average is ${avg}`;
-    return 'str';
+let account = {
+    name: 'Ihor',
+    surname: 'Nepipenko',
+    getFullName,
+
+};
+
+account.getFullName();
+
+
+interface IUElement {
+    addClickListener(onclick: (this: void, e: Event) => void): void;
 }
 
-average(1, '2');
-average('1', 1);
-average(1, 1, '1');
+class UIElement implements IUElement {
+    addClickListener(_onclick: (this: void, e: Event) => void): void {
 
-average('1', '2');
-average(1);
-average(1, 2);
-average(1, 2, 2, 3, 4, 5, 6, 5);
-average(1, '2', 2, '3', 4, 5, 6, 5);
-average(1, 2, 's');
-const num: Array = average(1, 2, 1);
-
-function sum(num: number) {
-    let currentSum = num;
-
-    function f(num2: number) {
-        currentSum += num2;
-        return f;
     }
-
-    f[Symbol.toPrimitive] = () => {
-        return currentSum;
-    };
-
-    return f;
 }
 
-const a = sum(1, 2, 3, 3, 4, 4, 5);
+
+class Handler {
+    info: string;
+
+    onClickBad(this: this, _: Event) {
+
+    }
+}
+
+let h = new Handler();
+let uiElement: UIElement = new UIElement();
+uiElement.addClickListener(h.onClickBad)
